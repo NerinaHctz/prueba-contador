82a8c89bf6042dc26e36b95017d2879f51dfc966
Nerina Castillo <nerina.cgomez@gmail.com>
2024-11-21 11:14:14 +0100

    implement Counter.jsx

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+export default function Counter() {
+
+    const [count, setCount] = useState(0)
+
+    const increment = () => setCount((prevState) => (prevState < 10 ? prevState + 1 : prevState))
+
+    const decrement = () => setCount((prevState) => (prevState > 0 ? prevState - 1 : prevState))
+
+    const reset = () => setCount(0)
+
+    return (
+        <>
+            <h3>Contador: <small>{count}</small></h3>
+
+            <div>
+                <button onClick={() => increment(1)}>Incrementar</button>
+                &nbsp;
+                <button onClick={() => decrement(1)}>Decrementar</button>
+                &nbsp;
+                <button onClick={() => reset()}>Reiniciar</button>
+            </div>
+        </>
+    )
+}
